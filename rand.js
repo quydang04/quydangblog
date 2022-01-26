cef9ba9552984ce98fd25c289bac55efff3a07ce
@quydang <81231812+quydang04@users.noreply.github.com>
2022-01-26 13:30:50 +0700

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,22 +1,12 @@
-alert("The project is in progress, not finished!");
 function getPass () {
-    let get = document.getElementById("slider").value;
-    let value = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-    let password = "";
-    for (var i = 0; i <= get ; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(){}|:/[]+=~";
+    var lengthPass = 15;
+    var password = "";
+    for (var i = 0; i <= lengthPass ; i++) {
+        var randPass = Math.floor(Math.random() * char.length);
+        password += char.substring(randPass, randPass + 1);
     }
-   document.getElementById("display").value = password;
-   document.getElementById("genpass").innerHTML += password + "<br>";
-   document.getElementById("length").innerHTML = "Length: 25";
-   document.getElementById("slider").oninput = function () {
-       if (document.getElementById("slider").value > 0) {
-        document.getElementById("length").innerHTML = "Length" + document.getElementById("slider").value;
-       }
-       else {
-        document.getElementById("length").innerHTML = "Length: 1";
-       }
-   }
+    document.getElementById("display").value = password;
 }
 function copyPass() {
    document.getElementById("display").select();
